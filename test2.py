d5e77b0c0faccc949da824270781287ccef24b67
Lutta Simiyu <lutta_muganda@hotmail.com>
2022-01-07 08:18:36 +1000

    First test of Maths package in Python

---
+++ test2.py
@@ -0,0 +1,12 @@
+import math
+x = 12; y = 144; z = 625   
+sq = math.pow (x, x)
+square_root = math.sqrt(y)
+sumOfSquares = math.sqrt(y + z)
+rootOfPi = math.sqrt(math.pi)
+print(sq)
+print(square_root)
+print(sumOfSquares)
+print(rootOfPi)
+
+
